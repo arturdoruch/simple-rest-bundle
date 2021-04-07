f60f046c8d6b07a66edf081f904c09f8a29fa62b
Artur Doruch <arturdoruch@interia.pl>
2021-04-07 04:07:08 +0200

    Create bundle services configuration in xml file

--- DependencyInjection/ArturDoruchSimpleRestExtension.php
+++ DependencyInjection/ArturDoruchSimpleRestExtension.php
@@ -2,10 +2,10 @@
 
 namespace ArturDoruch\SimpleRestBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
  * @author Artur Doruch <arturdoruch@interia.pl>
@@ -17,11 +17,11 @@ class ArturDoruchSimpleRestExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
 
         $container->setParameter('arturdoruch_simple_rest.api_paths', $config['api_paths']);
         $container->setParameter('arturdoruch_simple_rest.form_error_flatten_messages', $config['form_error_flatten_messages']);
